27936bf519bee8b05867817cd926f4d22ef5c105
Chandan <chandancvp08@gmail.com>
2023-09-16 15:58:43 +0530

    added routing

---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SafeAreaView, View, Text, Button} from 'react-native'
+
+function HomeScreen({ navigation }) {
+  return (
+   <SafeAreaView>
+       <View>
+              <Text>Your Current Score</Text>
+        </View>
+   </SafeAreaView>
+  )
+}
+
+export default HomeScreen
