be6d4f3d95176b0736e1b2744d04a43cc4eae0aa
Annihilator544 <94665572+Annihilator544@users.noreply.github.com>
2023-09-17 10:11:36 +0530

    Add files via upload

--- components/Form4.js
+++ components/Form4.js
@@ -7,16 +7,18 @@ import React, { useState } from 'react';
   import {useForm , Controller} from "react-hook-form";
 
   const data = [
-    { label: 'Economy Class', value: '1' },
-    { label: 'Premium Economy', value: '2' },
-    { label: 'Business Class', value: '3' },
-    { label: 'First Class', value: '4' },
-    { label: 'Average (Unknown Class)', value: '5' },
+    { label: 'Car', value: '1' },
+    { label: 'Bus', value: '2' },
+    { label: 'Flight', value: '3' },
+    { label: 'Train', value: '4' },
+    { label: 'Bike', value: '5' },
+    { label: 'Walk', value: '6' },
+    { label: 'Average (Unknown Value) ', value: '7' },
   ];
  
   const onSubmit = (data) => console.log(data)
 
-  const Form2 = () => {
+  const Form4 = () => {
     const {
         control,
         handleSubmit,
@@ -34,12 +36,48 @@ import React, { useState } from 'react';
     const[LandingLatitudeA2, setLandingLatitudeA2] = useState("");
     const[LandingLongitudeA2, setLandingLongitudeA2] = useState("");
     let data2=[StartLatitudeA1, StartLongitudeA1, LandingLatitudeA2, LandingLongitudeA2];
+    const[VehicleType, setVehicleType] = useState(0);
     // const geocoder = new Google.maps.Geocoder();
     return (
         
         
       <View style={styles.container}>
+      
       <View style={styles.container2}>
+      <View style={styles.Input}>
+        <Text>Vehicle Taken</Text>
+        <Dropdown
+          style={[styles.dropdown, isFocus && { borderColor: 'blue' }]}
+          placeholderStyle={styles.placeholderStyle}
+          selectedTextStyle={styles.selectedTextStyle}
+          inputSearchStyle={styles.inputSearchStyle}
+          iconStyle={styles.iconStyle}
+          data={data}
+          search
+          maxHeight={300}
+          labelField="label"
+          valueField="value"
+          placeholder={!isFocus ? 'Select Type' : '...'}
+          searchPlaceholder="Search..."
+          value={value}
+          onFocus={() => setIsFocus(true)}
+          onBlur={() => setIsFocus(false)}
+          onChange={item => {
+            setValue(item.value);
+            setIsFocus(false);
+            setVehicleType(item.value);
+          }}
+          renderLeftIcon={() => (
+            <AntDesign
+              style={styles.icon}
+              color={isFocus ? 'blue' : 'black'}
+              name="Safety"
+              size={20}
+            />
+          )}
+        />
+        </View>
+      {errors.firstName && <Text>This is required.</Text>}
       <Text >Start Location</Text>
       <GooglePlacesAutocomplete
       GooglePlacesDetailsQuery={{ fields: "geometry" }}
@@ -92,6 +130,7 @@ import React, { useState } from 'react';
         //get the lat and long
         
         console.log("data", data);
+        console.log(VehicleType);
         console.log("details", details);
         console.log(JSON.stringify(details?.geometry?.location));
         
@@ -106,12 +145,12 @@ import React, { useState } from 'react';
     />
       
       </View>
-        <Button style={styles.Button2} title="Submit" onPress={()=>{console.log(data2)}} />
+        <Button style={styles.Button2} title="Submit" onPress={()=>{console.log(data2); console.log(VehicleType)}} />
       </View>
     );
   };
 
-  export default Form2;
+  export default Form4;
 
   const styles = StyleSheet.create({
     Input: {
